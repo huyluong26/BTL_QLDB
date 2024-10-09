7da045185ba770839da2e41648fb80b09b699a07
huyluong26 <160562478+huyluong26@users.noreply.github.com>
2024-10-09 19:38:33 +0700

    Add files via upload

--- Match.java
+++ Match.java
@@ -1,76 +1,110 @@
+// Lớp Match - lớp cha  
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
 
-public abstract class Match {
-    //Attributes
-    private int matchId;
-    private String opponentName;
-    private String dateOfMatch;
-    private int numberOfGoals;
-    private int goalsConceded;
-    private String result;
-    Player [] players;
-
-    //Constructor
-    public Match(int matchId, String opponentName, String dateOfMatch, int numberOfGoals, int goalsConceded, String result) {
-        this.matchId = matchId;
-        this.opponentName = opponentName;
-        this.dateOfMatch = dateOfMatch;
-        this.numberOfGoals = numberOfGoals;
-        this.goalsConceded = goalsConceded;
-        this.result = result;
-
-    }
-    //Getter và Setter
-
-    public int getMatchId() {
-        return matchId;
+    public Match(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
     }
 
-    public void setMatchId(int matchId) {
-        this.matchId = matchId;
+
+
+
+    public void displayResult() {
+        System.out.println("Match Result: " + homeTeam + " " + homeScore + " - " + awayScore + " " + awayTeam);
+        if(homeScore > awayScore) {
+            System.out.println("Thắng");
+        } else if (awayScore > homeScore) {
+            System.out.println("Thua");
+        }else {
+            System.out.println("Hoà");
+        }
     }
 
-    public String getOpponentName() {
-        return opponentName;
+    // Getter methods  
+    public String getHomeTeam() {
+        return homeTeam;
     }
 
-    public void setOpponentName(String opponentName) {
-        this.opponentName = opponentName;
+    public String getAwayTeam() {
+        return awayTeam;
     }
 
-    public String getDateOfMatch() {
-        return dateOfMatch;
+    public int getHomeScore() {
+        return homeScore;
     }
 
-    public void setDateOfMatch(String dateOfMatch) {
-        this.dateOfMatch = dateOfMatch;
+    public int getAwayScore() {
+        return awayScore;
+    }
+    //Setter methods
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
     }
 
-    public int getNumberOfGoals() {
-        return numberOfGoals;
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
     }
 
-    public void setNumberOfGoals(int numberOfGoals) {
-        this.numberOfGoals = numberOfGoals;
+    public void setHomeScore(int homeScore) {
+            checkScore(homeScore);
+            this.homeScore = homeScore;
+
     }
 
-    public int getGoalsConceded() {
-        return goalsConceded;
+    public void setAwayScore(int awayScore) {
+        if(this.awayScore < 0){
+            throw new IllegalArgumentException("Số bàn thắng không thể nhỏ hơn 0!");
+        }
+        this.awayScore = awayScore;
     }
-
-    public void setGoalsConceded(int goalsConceded) {
-        this.goalsConceded = goalsConceded;
+    //Exceptions methods
+    public void checkScore(int homeScore) throws InvalidException {
+        if (homeScore < 0) {
+            throw new InvalidScoreException("Số bàn thắng không hợp lệ");
+        }
     }
+}
 
-    public String getResult() {
-        return result;
-    }
 
-    public void setResult(String result) {
-        this.result = result;
+
+
+
+// Lớp UclMatch - thừa kế từ Match  
+class UclMatch extends Match {
+    private String stadium;
+
+    public UclMatch(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        super(homeTeam, awayTeam, homeScore, awayScore);
+
     }
 
     @Override
-    public String toString() {
-        return super.toString();
+    public void displayResult() {
+        super.displayResult();
+        System.out.println("Stadium: " + stadium);
     }
 }
+
+// Lớp EplMatch - thừa kế từ Match  
+class EplMatch extends Match {
+    private String referee;
+
+    public EplMatch(String homeTeam, String awayTeam, int homeScore, int awayScore, String referee) {
+        super(homeTeam, awayTeam, homeScore, awayScore);
+        this.referee = referee;
+    }
+
+    @Override
+    public void displayResult() {
+        super.displayResult();
+        System.out.println("Referee: " + referee);
+    }
+}
+
+
